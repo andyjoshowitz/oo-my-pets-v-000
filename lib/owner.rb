a77641971f000c0376c6621992fd4a32a7dfb696
Andrew Joshowitz <ajoshow@sas.upenn.edu>
2018-02-14 16:52:17 +0000

    automatically backed up by learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,42 @@
 class Owner
-  # code goes here
+  attr_accessor :pets, :name, :mood
+  attr_reader :species
+  @@all = []
+
+  def initialize(species)
+    @pets = {fishes: [], cats: [], dogs: []}
+    @@all << self
+    @species = "human"
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.count
+    @@all.count
+  end
+
+  def self.reset_all
+    self.all.clear
+  end
+  
+  def say_species
+    return "I am a #{@species}."
+  end
+  
+  def buy_fish(name)
+    fish = Fish.new(name)
+    @pets[:fishes] << fish
+  end
+  
+  def buy_cat(name)
+    cat = Cat.new(name)
+    @pets[:cats] << cat
+  end
+  
+  def buy_dog(name)
+    dog = Dog.new(name)
+    @pets[:dogs] << dog
+  end
 end
